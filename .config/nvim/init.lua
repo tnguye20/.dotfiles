4899a73464dfbc3c460937cc25421e8dd8b536e0
Thang Nguyen <thangnguyenuvm@gmail.com>
2024-05-08 17:32:01 -0400

    Misc updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -868,6 +868,9 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.jump').setup()
+      require('mini.jump2d').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
